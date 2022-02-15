d2da91f29c3aaaa791aee020620a50eb63b2b59c
Doublondenier72 <ilhann.musi@mail.com>
2022-02-15 20:34:19 +0100

    save

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if(($configurate) == false ){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $websitename ?> - Accueil</title>
+    <title><?= $websitename ?> - Home</title>
     <link rel="shortcut icon" href="img/defaultfavicon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/styles.css">
@@ -29,10 +29,7 @@ if(($configurate) == false ){
          <div class="collapse navbar-collapse" id="navbarSupportedContent">
            <ul class="navbar-nav mr-auto">
              <li class="nav-item active">
-               <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-             </li>
-             <li class="nav-item">
-               <a class="nav-link" href="#">Link</a>
+               <a class="nav-link" href="./home">Home</a>
              </li>
              <li class="nav-item dropdown">
                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
@@ -44,9 +41,6 @@ if(($configurate) == false ){
                  <div class="dropdown-divider"></div>
                  <a class="dropdown-item" href="#">Something else here</a>
                </div>
-             </li>
-             <li class="nav-item">
-               <a class="nav-link disabled">Disabled</a>
              </li>
            </ul>
            <form class="form-inline my-2 my-lg-0">
